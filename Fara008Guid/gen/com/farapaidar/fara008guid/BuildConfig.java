133fb876d34d7e6a600055dec761ce83a5d4a7a6
Aref Abedjooy <ArshockAbedan@users.noreply.github.com>
2022-03-28 16:02:37 -0400

    Add files via upload

---
+++ Fara008Guid/gen/com/farapaidar/fara008guid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.farapaidar.fara008guid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Fara008Guid/src/com/farapaidar/fara008guid/MainActivity.java
@@ -0,0 +1,27 @@
+package com.farapaidar.fara008guid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Object c = java.util.UUID.randomUUID();
+		TextView tx = (TextView) findViewById(R.id.txt);
+		tx.setText(c.toString());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
